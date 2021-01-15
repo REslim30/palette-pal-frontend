7b8cd2a5a5eb4e3d6ab71ff2bf508ded446a7a6d
Aidan Do <giahuydo99@gmail.com>
2021-01-15 14:30:59 +1030

    Home page now links to SignUp

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -3,6 +3,10 @@ import { Button } from "@material-ui/core";
 import artVectorIllustration from "./art-vector-illustration.svg"
 
 export default function Home(props) {
+  const goToSignUp = function(event) {
+    window.location = '/signup/';
+  }
+
   return (
     <div className="max-w-screen-md mx-auto">
       <header className="flex justify-between items-center p-4">
@@ -17,14 +21,14 @@ export default function Home(props) {
         <article>
           <div className="flex flex-col items-center w-64 my-8 mx-auto">
             <h2 className="text-4xl text-center mb-4">Store all of your color palettes in one place.</h2>
-            <Button variant="contained" color="primary">Get Started</Button>
+            <Button onClick={goToSignUp} variant="contained" color="primary">Get Started</Button>
           </div>
           <img src={artVectorIllustration} className="w-4/5 mx-auto" alt="A person painting"></img>
         </article>
 
         <article className="bg-purple-50 py-8 px-4 mt-8 flex flex-col items-center">
           <h2 className="text-4xl mb-8">Save your color palettes for your next big project.</h2>
-          <Button variant="contained" color="primary">Get Started</Button>
+          <Button onClick={goToSignUp} variant="contained" color="primary">Get Started</Button>
         </article>
       </main>
     </div>
--- src/scenes/SignUp/index.js
+++ src/scenes/SignUp/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function SignUp(props) {
-  return <h1>Hello World from Signup</h1>
+  return <>
+    
+  </>
 }
