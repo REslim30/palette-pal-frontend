5b4ffef4085ac3e37cce8829d63f3f6b78ec2958
Aidan Do <giahuydo99@gmail.com>
2021-01-16 18:12:06 +1030

    AppBar for palette View

--- src/scenes/PaletteView/index.js
+++ src/scenes/PaletteView/index.js
@@ -1,5 +1,39 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  addButton: {
+    marginRight: '-12px',
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
 export default function PaletteView(props) {
-  return <h1>Hello World</h1>
+  const classes = useStyles();
+
+  return <>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <h1 className='text-xl flex-grow'>All</h1>
+        <IconButton edge="start" className={ classes.addButton } color="inherit" aria-label="Add Palette">
+          <AddIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  </>
 }
