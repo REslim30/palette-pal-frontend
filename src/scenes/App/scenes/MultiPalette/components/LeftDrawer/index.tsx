b1f9ed53a883516bcd7884462c55309f62931f71
Aidan Do <giahuydo99@gmail.com>
2021-01-28 16:46:10 +1030

    feat: group button hover states

--- src/scenes/App/scenes/MultiPalette/components/LeftDrawer/index.tsx
+++ src/scenes/App/scenes/MultiPalette/components/LeftDrawer/index.tsx
@@ -2,23 +2,45 @@ import React from "react";
 
 import Drawer from "@material-ui/core/Drawer";
 
-import { useGroups, setCurrentGroup } from "#app/services/app-state-store";
+import { useGroups, setCurrentGroup, useCurrentGroup } from "#app/services/app-state-store";
 import ColorBall from "#src/components/ColorBall/index";
 import { useMultiPaletteContext } from "../../services/MultiPaletteContext";
 
 export default function LeftDrawer(props: any) {
   const groups = useGroups();
+  const currentGroup = useCurrentGroup();
   const { leftDrawerOpen, setLeftDrawerOpen } = useMultiPaletteContext();
   
   const handleClose = () => {
     setLeftDrawerOpen(false);
   }
 
+  const selectedGroupStyle = (group: Group) => (
+    group.id === currentGroup?.id ? 'bg-neutral-300' : ''
+  );
+
   return <Drawer anchor="left" open={leftDrawerOpen} onClose={handleClose}>
-    {groups?.map((group) => (
-      <button className="w-64">
-        <ColorBall color={group.color || 'gray'}/>
-      </button>
-    ))}
+    <div className="bg-primary-500 h-24">
+      {/* TODO: Fill out profile section */}
+    </div>
+    <article>
+      <div className="px-6 mt-4 mb-2">
+        <h2 className="text-3xl">
+          Groups
+        </h2>
+      </div>
+
+      {groups?.map((group) => (
+        <button 
+          className={`w-64 grid gap-6 items-center justify-start pl-6 py-2 hover:bg-neutral-100 ${selectedGroupStyle(group)}`} 
+          style={{ gridTemplateColumns: "min-content 1fr" }} 
+          key={group.id}>
+          <ColorBall color={group.color || 'gray'}/>
+          <span className="text-left mt-0.5 font-sans text-lg">
+            {group.name}
+          </span>
+        </button>
+      ))}
+    </article>
   </Drawer>;
 };
