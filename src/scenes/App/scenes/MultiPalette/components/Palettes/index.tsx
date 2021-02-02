1df2b1e62c5222badcd576e082b8c57f5dd5a6db
Aidan Do <giahuydo99@gmail.com>
2021-02-02 14:17:11 +1030

    feat: Initial message positioned in the middle

---
+++ src/scenes/App/scenes/MultiPalette/components/Palettes/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import CircularProgress from "@material-ui/core/CircularProgress"
+import { useCurrentGroup, usePalettes } from "#src/services/app-state-store";
+import PaletteCard from "./components/PaletteCard/index";
+import PickingPalettesIllustration from "#src/images/picking-palettes.svg";
+
+export default function Palettes(props: unknown) {
+  const allPalettes = usePalettes();
+  const group = useCurrentGroup();
+
+  let palettes = group?.palettes || allPalettes;
+
+  if (!palettes)
+    return <CircularProgress />;
+
+  if (!palettes.length)
+    return <main className="flex flex-col items-center justify-center flex-grow">
+      <span className="text-xl text-neutral-800">Click '+' to create a palette!</span>
+    </main>
+  else 
+    return <main className="grid gap-4 p-6" style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(160px, 1fr))' }}>
+      { palettes.map(palette => <PaletteCard palette={palette} key={palette.id}/>) }
+    </main>;
+}
--- src/scenes/App/scenes/MultiPalette/index.tsx
+++ src/scenes/App/scenes/MultiPalette/index.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
-import PaletteCard from "./components/PaletteCard/index";
 import { RouteComponentProps } from '@reach/router';
-
-
-import CircularProgress from "@material-ui/core/CircularProgress"
-import { useCurrentGroup, usePalettes } from "#src/services/app-state-store";
 
 import MultiPaletteAppBar from "./components/MultiPaletteAppBar/index";
 import LeftDrawer from "./components/LeftDrawer/index";
 import MultiPaletteContext from './services/MultiPaletteContext';
 import GroupCreator from "./components/GroupCreator/index";
+import Palettes from "./components/Palettes/index";
 
 interface MultiPaletteProps extends RouteComponentProps {
   groupId?: number;
@@ -26,28 +22,14 @@ export default function MultiPalette(props : MultiPaletteProps) {
       groupToEdit,
       setGroupToEdit
     }}>
-    <MultiPaletteAppBar/>
-    <Palettes/>
-    <LeftDrawer /> 
-    <GroupCreator />
+    <div className="flex flex-col min-h-screen">
+      <MultiPaletteAppBar/>
+      <Palettes/>
+      <LeftDrawer /> 
+      <GroupCreator />
+    </div>
   </MultiPaletteContext.Provider>
 }
 
 
 
-function Palettes(props: unknown) {
-  const allPalettes = usePalettes();
-  const group = useCurrentGroup();
-
-  let palettes = group?.palettes || allPalettes;
-
-  if (!palettes)
-    return <CircularProgress />;
-
-  if (!palettes.length)
-    return <p>You have no palettes. Click on '+' to create your first palette!</p>
-  else 
-    return <main className="grid gap-4 p-6" style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(160px, 1fr))' }}>
-      { palettes.map(palette => <PaletteCard palette={palette} key={palette.id}/>) }
-    </main>;
-}
